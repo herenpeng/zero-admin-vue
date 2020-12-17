8be8f79e81286a182b5aad7dbe6dc82aebfb04d7
herenpeng <2519832582@qq.com>
2020-12-17 22:26:16 +0800

    一个通用的后台管理系统，使用Vue+ElementUI开发--优化系统

--- vue.config.js
+++ vue.config.js
@@ -13,7 +13,7 @@ const name = defaultSettings.title || 'zero-admin-vue' // page title
 // For example, Mac: sudo npm run
 // You can change the port by the following methods:
 // port = 9528 npm run dev OR npm run dev --port = 9528
-const port = process.env.port || process.env.npm_config_port || 9001 // dev port
+const port = process.env.port || process.env.npm_config_port || 20000 // dev port
 
 // All configuration item explanations can be find in https://cli.vuejs.org/config/
 module.exports = {
