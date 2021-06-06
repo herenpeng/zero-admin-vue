1145a5aa8d62eb983b2fa248a1da7e78d4a5ad0b
herenpeng <2519832582@qq.com>
2021-06-06 14:14:24 +0800

    :sparkles:系统配置功能开发

--- src/layout/index.vue
+++ src/layout/index.vue
@@ -11,7 +11,7 @@
       <right-panel v-if="showSettings">
         <settings />
       </right-panel>
-      <div class="footer">赣ICP备2020014777号</div>
+      <div class="footer">赣ICP备2021xxxxxxx号</div>
     </div>
   </div>
 </template>
@@ -104,5 +104,6 @@ export default {
     line-height: 30px;
     text-align: center;
     background-color: #304156;
+    color: white;
   }
 </style>
